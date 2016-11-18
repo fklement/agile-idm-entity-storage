28565d103907b12ca2a649858f57e2f293cd2c7d
david <nopbyte>
2016-11-18 18:29:51 +0100

    include tests to ensure that looking up entities for more than attribute returns the entity only when all the attribs. match

--- tests/level-storage-test.js
+++ tests/level-storage-test.js
@@ -244,7 +244,7 @@ describe('LevelStorage', function () {
 
     });
 
-    it('should resolve one data by an id and type, if it has been previously stored', function (done) {
+    it('should resolve one data when looking for the one attribute value and type, if it has been previously stored', function (done) {
       var storage = createLevelStorage();
       var owner = "1";
       var entity_id = "2";
@@ -278,6 +278,88 @@ describe('LevelStorage', function () {
               }
             }
 
+          });
+      }, function rej(r) {
+        console.error('a' + r);
+        throw r;
+      })
+
+    });
+
+    it('should resolve one data when looking for more than one attribute value and type, if it has been previously stored', function (done) {
+      var storage = createLevelStorage();
+      var owner = "1";
+      var entity_id = "2";
+      var entity_type = "user";
+      var data = {
+        "name": "string",
+        "token": "123"
+      };
+      var datasecond = {
+        "name": "mysecond attribute",
+        "token": "123"
+      };
+
+      var p = storage.createEntityPromise(entity_id, entity_type, owner, data);
+      p.then(function (d) {
+        return storage.createEntityPromise("otherentity", entity_type, owner, datasecond);
+      }).then(function (created) {
+        storage.listEntitiesByAttributeValueAndType([{
+            attribute_type: "name",
+            attribute_value: "string"
+          },{
+            attribute_type: "token",
+            attribute_value: "123"
+          }])
+          .then(function (result) {
+            if (result.length == 1) {
+              result = result[0];
+              if (result.id == entity_id && result.type == entity_type && result.owner == owner) {
+                delete result.id; //id is included so remove it to check
+                delete result.type; //entity type is included so remove it to check
+                delete result.owner; //owner is included so remove it to check
+                if (deepdif.diff(data, result) == undefined)
+                  storage.cleanDb(done);
+              }
+            }
+
+          });
+      }, function rej(r) {
+        console.error('a' + r);
+        throw r;
+      })
+
+    });
+
+    it('should resolve with empty array when looking when looking for more than one attribute value and type and there is an entity that has only one attribute type with the value used for the search', function (done) {
+      var storage = createLevelStorage();
+      var owner = "1";
+      var entity_id = "2";
+      var entity_type = "user";
+      var data = {
+        "name": "string",
+        "token": "123"
+      };
+      var datasecond = {
+        "name": "mysecond attribute",
+        "token": "123"
+      };
+
+      var p = storage.createEntityPromise(entity_id, entity_type, owner, data);
+      p.then(function (d) {
+        return storage.createEntityPromise("otherentity", entity_type, owner, datasecond);
+      }).then(function (created) {
+        storage.listEntitiesByAttributeValueAndType([{
+            attribute_type: "name",
+            attribute_value: "string"
+          },{
+            attribute_type: "token",
+            attribute_value: "non-equal-to-123"
+          }])
+          .then(function (result) {
+            if (result.length === 0)
+                  storage.cleanDb(done);
+            
           });
       }, function rej(r) {
         console.error('a' + r);
